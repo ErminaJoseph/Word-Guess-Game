de956ae154791ad98345ae088c5c28b4847449ae
Ermina Joseph <ermina.joseph@gmail.com>
2019-10-31 18:37:43 -0400

    adding fuction to display blank spaces

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,32 @@
 var theWords = ["euphoria", "insecure", "gameofthrones", "ballers", "watchmen", "vice", "siliconvalley"];
 var wins = 0;
-var lettersGuessed = [];
+var correctLettersGuessed = [];
+var incorrectLettersGuessed = [];
 var numberOfGuesses = 20;
 var selectedWord = theWords[Math.floor(Math.random() * theWords.length)];
 var splitWord = selectedWord.split("");
+var blankSpace = [];
 
 
-
-
+function intialDisplay(display) {
+    for (var h = 0; h < splitWord.length; h++) {
+        wordSpace = document.getElementById("word-space")
+        wordSpace.textContent = blankSpace.push("_");
+    }
+}
 
 document.onkeyup = function(event) {
     var userChoice = event.key;  
 
+    var getStarted = document.getElementById("get-started");
+    getStarted.textContent = "";
+
     for (var i = 0; i < splitWord.length; i++) {
         if (userChoice === splitWord[i].charAt(0)) {
-            console.log(splitWord[i]);
+            var wordSpace = document.getElementById("word-space");
+            wordSpace.textContent = correctLettersGuessed.push(userChoice);
         }
+
     }
     
     
